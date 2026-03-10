59eef14849d4a2589158ad2a24d5259fc049cbff
NEDMInsane <truxaln@gmail.com>
2026-03-09 20:04:39 -0400

    started working with saving conversations locally. (Conversation file is bogus)

--- gui_chat.py
+++ gui_chat.py
@@ -1,6 +1,9 @@
+import fileio
 import gui_interface, model_interface
 
 if __name__ == '__main__':
-    model = model_interface.ModelInterface()
-    app = gui_interface.GuiApplication(model=model)
-    app.mainloop()
+    #model = model_interface.ModelInterface()
+    #app = gui_interface.GuiApplication(model=model)
+    #app.mainloop()
+
+    print(fileio.read_conversation_file('conversations/2026-03-05--14-17-21'))
